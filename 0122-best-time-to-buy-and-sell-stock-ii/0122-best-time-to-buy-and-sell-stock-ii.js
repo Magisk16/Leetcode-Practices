4692ce3024770e6cb828f84192daab60e2b233bd
Magisk16 <122012249+Magisk16@users.noreply.github.com>
2023-01-26 18:32:53 +0530

    Time: 96 ms (26.40%), Space: 42.6 MB (30.64%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -5,12 +5,11 @@
 var maxProfit = function(arr) {
     let arr1 = [...arr];
 let arr2 = [...arr];
-let decreasing =arr1.sort((a,b)=>b-a);
-let increasing = arr2.sort((a,b)=>a-b);
+// let decreasing =arr1.sort((a,b)=>b-a);7
 let profit=0;
-if(JSON.stringify(arr) == JSON.stringify(decreasing)){
-    return 0;
-}else{
+// if(JSON.stringify(arr) == JSON.stringify(decreasing)){
+//     return 0;
+// }else{
     for(let i=1;i<arr.length;i++){
         if(arr[i]>arr[i-1]){
             profit+=(arr[i]-arr[i-1]);
@@ -18,5 +17,5 @@ if(JSON.stringify(arr) == JSON.stringify(decreasing)){
     }
     return profit
 
-}   
+//}   
 };
