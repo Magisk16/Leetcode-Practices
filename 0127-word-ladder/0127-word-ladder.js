3402598b37068ecddc294745451cf2ecce040651
Magisk16 <122012249+Magisk16@users.noreply.github.com>
2023-02-21 18:59:51 +0530

    Time: 216 ms (82.32%), Space: 48.6 MB (85.55%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+  let wordSet = new Set(wordList);
+  let queue =[];
+  queue.push({word: beginWord, pathLen: 1});
+    while(queue.length>0){
+        let {word, pathLen} = queue.shift();
+        if(word==endWord){
+            return pathLen;
+        }
+        for(let i=0;i<word.length;i++){
+            let prefix = word.substring(0,i);
+            let suffix = word.substring(i+1);
+            for(let i=0;i<26;i++){
+                let newStr = prefix+ String.fromCharCode(97+i) +suffix;
+                if(wordSet.has(newStr)){
+                    queue.push({word: newStr, pathLen: pathLen+1});
+                    wordSet.delete(newStr);
+                }
+            }
+            
+        }
+    }
+    return 0;                                                                                                                                                                                                                       
+    
+};
